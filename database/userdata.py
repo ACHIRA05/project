525c7f5c20991d4da7a5a441bdb40adac8d442d8
achirapo <achira.p@kkumail.com>
2025-10-21 04:44:40 +0700

    update

--- database/userdata.py
+++ database/userdata.py
@@ -1,9 +1,15 @@
-import sqlite3
-
+import sqlite3,os
+print('ABS PATH =', os.path.abspath('Userdata.db')) 
 conn = sqlite3.connect('Userdata.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL)''')
+c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        phone TEXT UNIQUE NOT NULL,
+        profile_image BLOB)""")
+conn.commit()
+conn.close()
